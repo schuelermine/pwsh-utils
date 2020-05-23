f6afe1df02780b490e5c467d4ebf2e431c1215ea
Anselm Schüler <anselmschueler@gmail.com>
2020-05-23 17:49:18 +0200

    more documentation and capitalization

--- for+.ps1
+++ for+.ps1
@@ -4,12 +4,31 @@ For+ acts as a python-esque alternative to PowerShell for syntax.
 In contrast to foreach, for+ allows access to the current index and the current item.
 The supplied script block can acces the current item using $_ and the current index using $i.
 
-.Parameter Array
+.PARAMETER Array
 The array to iterate through.
 
-.Parameter ScriptBlock
+.PARAMETER ScriptBlock
 The script block that is executed for each item in the array.
 User $_ to refer to the current item and $i to refer to that item's index.
+
+.EXAMPLE
+> for+ ((1,2),(3,4)) {$_[$i]}
+1
+4
+
+.EXAMPLE
+> for+ ('a'..'c') {"$_" * $i}
+
+b
+cc
+
+.EXAMPLE
+> for+ (,('a'..'e')*5) {$_[$i]}
+a
+b
+c
+d
+e
 #>
 
 function For+ {    
