c99142a76e2af01d8dc40032de01c9f6142c9483
Anselm Schüler <git@anselmschueler.com>
2020-06-05 20:18:22 +0200

    Update Get-ShortCommands.psm1

--- Get-ShortCommands.psm1
+++ Get-ShortCommands.psm1
@@ -1,28 +1,56 @@
 function Get-ShortCommands {
+    [CmdletBinding()]param()
+
     foreach ($Command in Get-Command) {
-        $Verb = $Command.Verb -as [Char[]]
-        $Noun = $Command.Noun -as [Char[]]
+        $Name = $Command.Name
+
+        $Verb, $Noun =
+            $Name -split "-", 2 -as [Char[][]]
+        
+        Write-Debug "Values:"
+        Write-Debug " $Name"
+        Write-Debug " $Verb"
+        Write-Debug " $Noun"
 
         $ShortVerb, $ShortNoun = "", ""
 
         if ($Verb.Count -ne 0) {
+            Write-Debug "Verb nonempty."
+
             foreach ($Char in $Verb) {
+                Write-Debug "Current item:"
+                Write-Debug " $Char"
+
                 if ([Char]::IsUpper($Char)) {
                     $ShortVerb += $Char
                 }
+
+                Write-Debug "Short verb:"
+                Write-Debug " $ShortVerb"
             }
         }
 
         if ($Noun.Count -ne 0) {
+            Write-Debug "Verb nonempty."
+
             foreach ($Char in $Noun) {
+                Write-Debug "Current item:"
+                Write-Debug " $Char"
+
                 if ([Char]::IsUpper($Char)) {
                     $ShortNoun += $Char
                 }
+
+                Write-Debug "Short noun:"
+                Write-Debug " $ShortNoun"
             }
         }
 
-        $ShortName =
-            @($ShortVerb, $ShortNoun).Where({$_.Length -ne 0}) -join "-"
+        $ShortName = 
+            Join-String* @($ShortVerb, $ShortNoun) "-"
+
+        Write-Debug "Short name:"
+        Write-Debug " $ShortName"
 
         [PSCustomObject]@{
             Name = $Command.Name
@@ -30,3 +58,14 @@ function Get-ShortCommands {
         }
     }
 }
+
+function Join-String* {
+    param(
+        [String[]]$Array,
+        [String]$Seperator
+    )
+
+    $Array.Where({$_.Length -ne 0}) -join "-"
+}
+
+Export-ModuleMember -Function "Get-ShortCommands"
